6f488d46120c4551188182a04e72bfdcaef0b950
NeoDev <neoforevershog@gmail.com>
2025-02-17 08:27:14 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ extras_require = {
 # Setup configuration
 setup(
     name="rvc_cli",
-    version="1.3.0",
+    version="1.4.0",
     packages=find_packages(include=["rvc_cli", "rvc_cli.*"]),
     install_requires=install_requires,
     extras_require=extras_require,
